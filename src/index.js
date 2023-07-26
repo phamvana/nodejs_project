478d6aed67e6068b85f3f0ab8ba953a0f7db0857
Pham Van A <phamvana@gmail.com>
2023-07-26 17:10:11 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//sattic --> xử lý file tĩnh
+app.use(express.static(path.join(__dirname,'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs',handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resourse\\views'));
+
+
+app.get('/',(req,res)=>{
+    return res.render('home');
+});
+
+
+app.get('/new',(req,res)=>{
+    return res.render('view');
+});
+
+app.listen(port, ()=>console.log(`Trang web ddang chay http://localhost:${port}`));
